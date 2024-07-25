9de28051bc11a8010ef7fcd87c3cbd443e336550
sainiavi <avisaini063@gmail.com>
2024-07-25 20:07:45 +0530

    basic frontend for asvaswap

---
+++ src/constants/chains.js
@@ -0,0 +1,22 @@
+export const networks = [3,4,5,42,123, 1337, 444800, 444900,80001, 97]
+// export const networks = [80001]
+
+export const ChainId = {
+ 
+  BSC: 97
+};
+
+export const routerAddress = new Map();
+// routerAddress.set(ChainId.MAINNET, "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
+// routerAddress.set(ChainId.ROPSTEN, "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
+// //routerAddress.set(ChainId.RINKEBY, "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
+// routerAddress.set(ChainId.RINKEBY, "0xF66c4c1d8e3eB69148483c0eb6dc225043579D49");
+// routerAddress.set(ChainId.GÖRLI, "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
+// routerAddress.set(ChainId.KOVAN, "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
+// routerAddress.set(ChainId.DEVNET, "0x04e555283D37aE85F6eB311fe2578F3B3f3dFc52");
+// routerAddress.set(ChainId.AUTONITY, "0x04e555283D37aE85F6eB311fe2578F3B3f3dFc52");
+// routerAddress.set(ChainId.PARASTATE, "0x07a1905D44feeA439ceFAabd11a63bEf684abE11");
+// routerAddress.set(ChainId.GANCHE, "0x0F44AC51198D8F99847db6C431448dBC673428f1");
+routerAddress.set(ChainId.BSC, "0x53635EfdEA37Ee0C723c587311e633b5FD2a04Fc");
+
+
---
+++ src/constants/deadline.js
@@ -0,0 +1,4 @@
+const deadline = Math.floor(Date.now()/1000 + (10*60))
+
+
+console.log(deadline)
